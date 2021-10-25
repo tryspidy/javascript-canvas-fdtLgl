689ca6b6b34929ccce2432941de5a80bd1485b28
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-26 01:31:16 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+#this code creates a circle within a canvas that must be created in HTML
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
